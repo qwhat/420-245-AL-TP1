a21108df1c811263fad40f140fb6d3271562713a
Justin Mao <justinmao@live.ca>
2024-03-01 16:33:51 -0500

    Ajout de commentaires et nettoiement du code

--- Configuration.java
+++ Configuration.java
@@ -1,17 +1,13 @@
 import java.text.DecimalFormat;
 
 public class Configuration {
+    /* Déclaration des variables */
     private String description;
     private double prixMax;
-
     public final int COMPOSANTS_MAX = 20;
     public Composant[] composants = new Composant[COMPOSANTS_MAX];
 
-
-    public String deuxDecimaux(double montant) {
-        DecimalFormat d = new DecimalFormat("0.00");
-        return d.format(montant);
-    }
+    /* Constructeur */
     public Configuration(String description, double prixMax, Composant[] composants) {
         this.description = description;
         this.prixMax = prixMax;
@@ -25,8 +21,33 @@ public class Configuration {
 
         }
     }
+
+    /* Get et Set */
+    public String getDescription() {
+        return this.description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getPrixMax() {
+        return this.prixMax;
+    }
+    public void setPrixMax(double prixMax) {
+        this.prixMax = prixMax;
+    }
+    public int getNbComposants(){ //retourne le nombre de composants non-null
+        int longueurTableau = 0;
+        for (int i = 0; i < this.composants.length; i++) {
+            if (composants[i] != null) {
+                longueurTableau++;
+            }
+        }
+        return longueurTableau;
+    }
+
+    /* Crée une copie de la Configuration */
     public Configuration copier() {
-        String description = this.description + " (copie)";
+        String description = getDescription() + " (copie)";
         double prixMax = getPrixMax();
         Composant[] composant = new Composant[this.composants.length];
 
@@ -40,7 +61,9 @@ public class Configuration {
         return new Configuration(description, prixMax, composant);
 
     }
-    public void  afficherTout() {
+
+    /* Affiche la Configuration et ses composants */
+    public void afficherTout() {
         Composant[] c = new Composant[getNbComposants()];
         int indexC = 0;
 
@@ -56,27 +79,8 @@ public class Configuration {
             System.out.println((i + 1) + ": " + c[i].toString() + " (" + deuxDecimaux(c[i].getPrix()) + "$)");
         }
     }
-    public String getDescription() {
-        return this.description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public double getPrixMax() {
-        return this.prixMax;
-    }
-    public void setPrixMax(double prixMax) {
-        this.prixMax = prixMax;
-    }
-    public int getNbComposants(){
-        int longueurTableau = 0;
-        for (int i = 0; i < this.composants.length; i++) {
-            if (composants[i] != null) {
-                longueurTableau++;
-            }
-        }
-        return longueurTableau;
-    }
+
+    /* Fait le prix total d'une Configuration selon un montant de taxe donné */
     public double calculerTotal(double taxe) {
         double prixTotale = 0.0;
         for (int i = 0; i < this.composants.length; i++) {
@@ -87,6 +91,8 @@ public class Configuration {
         }
         return prixTotale + (prixTotale * taxe);
     }
+
+    /* Trouve le premier Composant avec la catégorie indiquée, sinon, retourne null */
     public Composant rechercher(String categorie) {
 
         for (int i = 0; i < composants.length;i++) {
@@ -99,6 +105,9 @@ public class Configuration {
         }
         return null;
     }
+
+    /* Permet d'ajouter un Composant donné à la Configuration si un composant de
+    cette catégorie n'y existe pas déjà et si le prix total restera en dessous ou égal au prix maximum */
     public boolean ajouter(Composant composant) {
         if (this.getNbComposants() < COMPOSANTS_MAX && this.rechercher(composant.categorie) == null && (calculerTotal(0.0) + composant.getPrix() <= getPrixMax() || getPrixMax() == 0) ) {
             for (int i = 0; i < COMPOSANTS_MAX; i++) {
@@ -112,19 +121,22 @@ public class Configuration {
         } else if (this.rechercher(composant.categorie) != null) {
             System.out.println("Il y a déjà un autre composant de cette catégorie:" + this.rechercher(composant.categorie).toString());
 
-        } else if (calculerTotal(0.0) + composant.getPrix() > getPrixMax() ) {
+        } else if (calculerTotal(0.0) + composant.getPrix() > getPrixMax()) {
             System.out.println("L'ajout de ce composant ferait dépasser le prix maximum: " + composant.toString());
 
         }
 
         return false;
     }
+
+    /* Permet de retirer un Composant donné d'une Configuration s'il est présent */
     public boolean retirer(Composant composant) {
         if (this.rechercher(composant.categorie) != null) {
             for (int i = 0; i < this.composants.length; i++) {
                 if (this.composants[i] == null) {
                     continue;
                 }
+
                 if (this.composants[i].estPareilQue(composant)) {
                     this.composants[i] = null;
                     System.out.println(composant.toString() + " retiré de la configuration (total=" + deuxDecimaux(calculerTotal(0.0)) +"$)");
@@ -132,9 +144,12 @@ public class Configuration {
                 }
             }
         }
+
         System.out.println("Composant introuvable: " + composant.toString());
         return false;
     }
+
+    /* Permet d'ajouter un Composant donné d'une Configuration en y enlevant le composant de la même catégorie */
     public boolean remplacer(Composant composant) {
 
         for (Composant c : this.composants) {
@@ -151,5 +166,11 @@ public class Configuration {
         }
         System.out.println("Il n'y a pas de composant de la catégorie " + composant.categorie);
         return false;
+    }
+
+    /* Fonction d'aide pour formatter les prix avec deux chiffres décimaux */
+    private String deuxDecimaux(double montant) {
+        DecimalFormat d = new DecimalFormat("0.00");
+        return d.format(montant);
     }
 }
